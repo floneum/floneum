e673108788beaa276da570c30489741fecec750a
Evan Almloff <evanalmloff@gmail.com>
2023-07-10 16:59:15 -0500

    add file system IO nodes

---
+++ plugins/write_to_file/src/lib.rs
@@ -0,0 +1,16 @@
+use std::io::Write;
+
+use floneum_rust::*;
+
+#[export_plugin]
+/// Writes some text to a file at the given path (in the /sandbox directory)
+pub fn write_to_file(
+    /// The text to write to the file
+    text: String,
+    /// The path to the file to write to
+    file_path: String,
+) {
+    let file_path = std::path::PathBuf::from(file_path);
+    let mut file = std::fs::File::create(file_path).unwrap();
+    file.write_all(text.as_bytes()).unwrap();
+}
