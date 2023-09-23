78e86333c3bed08cec07988878044d547379bf59
Evan Almloff <evanalmloff@gmail.com>
2023-09-23 17:17:36 -0500

    make output of whisper example more readable

--- floneumin/examples/transcribe.rs
+++ floneumin/examples/transcribe.rs
@@ -4,15 +4,15 @@ use tokio::time::{Duration, Instant};
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let mut model = WhisperBuilder::default()
-        .model(WhichModel::SmallEn)
+        .model(WhichModel::LargeV2)
         .build()?;
 
-    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
+    let (tx, mut rx) = tokio::sync::mpsc::channel(5);
     std::thread::spawn(move || {
         tokio::runtime::Runtime::new()
             .unwrap()
             .block_on(async move {
-                let recording_time = Duration::from_secs(20);
+                let recording_time = Duration::from_secs(10);
                 loop {
                     let input = floneumin_sound::source::mic::MicInput::default()
                         .record_until(Instant::now() + recording_time)
@@ -22,18 +22,27 @@ async fn main() -> Result<(), anyhow::Error> {
                 }
             })
     });
+    
+    let mut current_time_stamp = 0.0;
 
+    println!("starting transcription loop");
     loop {
-        println!("recording");
+        let mut next_time_stamp = current_time_stamp;
         let input = rx.recv().await.unwrap();
-
-        println!("transcribing");
-
-        let start_time = Instant::now();
 
         let transcribed = model.transcribe(input)?;
 
-        println!("transcribed: {:?}", transcribed);
-        println!("took: {:?}", start_time.elapsed());
+        for transcribed in transcribed {
+            if transcribed.probability_of_no_speech() < 0.90 {
+                let start = current_time_stamp + transcribed.start();
+                let end = start + transcribed.duration();
+                next_time_stamp = end;
+                
+                let text = transcribed.text();
+                println!("({:01} - {:01}): {}", start, end, text);
+            }
+        }
+        
+        current_time_stamp = next_time_stamp;
     }
 }
