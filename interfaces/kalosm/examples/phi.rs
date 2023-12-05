5248f763166b1fa6a23513de18ee0c5332f502de
Evan Almloff <evanalmloff@gmail.com>
2023-12-05 07:10:45 -0600

    simplify phi example

--- interfaces/kalosm/examples/phi.rs
+++ interfaces/kalosm/examples/phi.rs
@@ -1,20 +1,14 @@
-use std::io::Write;
-
-use futures_util::stream::StreamExt;
 use kalosm_language::*;
-use kalosm_streams::TextStream;
 
 #[tokio::main]
 async fn main() {
-    let mut llm = Phi::start().await;
-    let prompt = "The following is a 300 word essay about why the capital of France is Paris:";
-    print!("{}", prompt);
-
-    let stream = llm.stream_text(prompt).with_max_length(1000).await.unwrap();
-
-    let mut sentences = stream.words();
-    while let Some(text) = sentences.next().await {
-        print!("{}", text);
-        std::io::stdout().flush().unwrap();
-    }
+    Phi::start()
+        .await
+        .stream_text("The capital of Paris is ")
+        .with_max_length(1000)
+        .await
+        .unwrap()
+        .to_std_out()
+        .await
+        .unwrap();
 }
