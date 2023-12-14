6248a22359cee07f2598b882500ba502f2ae2ba2
Evan Almloff <evanalmloff@gmail.com>
2023-12-14 09:52:43 -0600

    fix formatting

--- interfaces/kalosm/examples/constrained-rust-types.rs
+++ interfaces/kalosm/examples/constrained-rust-types.rs
@@ -2,7 +2,7 @@ use kalosm_language::*;
 
 #[tokio::main]
 async fn main() {
-    let llm =Phi::v2().unwrap();
+    let llm = Phi::v2().unwrap();
     let prompt = "Realistic mock user names for a chat application: ";
 
     let validator = <[Word<1, 10>; 10] as HasParser>::new_parser();
