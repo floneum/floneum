3642d4eb5717ea3245edf1d1105f0bb9df9a9117
Evan Almloff <evanalmloff@gmail.com>
2024-01-19 20:31:58 -0600

    fix prompt annealing

--- interfaces/kalosm/src/prompt_annealing.rs
+++ interfaces/kalosm/src/prompt_annealing.rs
@@ -10,7 +10,7 @@ use rand::{
 
 use crate::{BertDistance, Metric, TestCases};
 
-pub struct PromptAnnealerBuilder<'a, M: Model, Met: for<'b> Metric<&'b str> = BertDistance>
+pub struct PromptAnnealerBuilder<'a, M: Model, Met: Metric<String> = BertDistance>
 where
     <<M as Model>::SyncModel as SyncModel>::Session: Sync + Send,
 {
@@ -43,7 +43,7 @@ where
     }
 }
 
-impl<'a, M: Model, Met: for<'b> Metric<&'b str>> PromptAnnealerBuilder<'a, M, Met>
+impl<'a, M: Model, Met: Metric<String>> PromptAnnealerBuilder<'a, M, Met>
 where
     <<M as Model>::SyncModel as SyncModel>::Session: Sync + Send,
 {
@@ -67,18 +67,18 @@ where
         self
     }
 
-    pub async fn build(self) -> PromptAnnealer<'a, M> {
+    pub async fn build(mut self) -> PromptAnnealer<'a, M, Met> {
         let (train_set, test_set) = if self.test.is_empty() {
             tracing::warn!("No test set provided, using a subset of the train set for evaluation");
 
-            let split = (self.train.len() / 5).max(1);
+            let split = (self.train.len() / 3).max(1);
 
             assert!(
-                split < self.train.len(),
+                split < self.train.len() || split < 1,
                 "Train set is too small to split into train and test sets. Provide more examples."
             );
 
-            (&self.train[..split], &self.train[split..])
+            (&self.train[split..], &self.train[..split])
         } else {
             (self.train, self.test)
         };
@@ -96,14 +96,15 @@ where
         let mut rng = rand::thread_rng();
         for _ in 0..self.initial_population {
             let amount = rng.gen_range(self.initial_choice_range.clone());
-            let index_vec = sample(&mut rng, train_set.len(), amount);
+            let index_vec = sample(&mut rng, train_set.len(), amount, );
+            let index_vec = index_vec.iter().collect::<Vec<_>>();
 
-            let mut remaining_cases = train_set.to_vec();
             let mut chosen_cases = train_set.to_vec();
-
-            for index in index_vec {
-                chosen_cases.push(remaining_cases.remove(index));
+            
+            for index in &index_vec {
+                chosen_cases.push(train_set[*index]);
             }
+            let  remaining_cases = train_set.iter().enumerate().filter(|(i,_)| !index_vec.contains(i)).map(|(_,x)| *x).collect::<Vec<_>>();
 
             population.push(
                 ExamplesInstance::new(
@@ -111,7 +112,8 @@ where
                     chosen_cases,
                     remaining_cases,
                     self.initial_temperature,
-                    self.test,
+                    test_set,
+                    &mut self.metric
                 )
                 .await,
             );
@@ -119,18 +121,20 @@ where
 
         PromptAnnealer {
             llm: self.llm,
-            train: self.train,
-            test: self.test,
+            train: train_set,
+            test: test_set,
             population,
+            metric: self.metric
         }
     }
 }
 
-pub struct PromptAnnealer<'a, M: Model>
+pub struct PromptAnnealer<'a, M: Model, Met: Metric<String> = BertDistance>
 where
     <<M as Model>::SyncModel as SyncModel>::Session: Sync + Send,
 {
     llm: &'a mut M,
+    metric: Met,
     train: &'a [(&'static str, &'static str)],
     test: &'a [(&'static str, &'static str)],
     population: Vec<ExamplesInstance>,
@@ -143,7 +147,7 @@ where
     pub async fn run(&mut self) {
         loop {
             for instance in &mut self.population {
-                instance.mutate(self.test, self.llm).await;
+                instance.mutate(self.test, self.llm, &mut self.metric).await;
             }
 
             if self.population[0].temperature < 0.10 {
@@ -172,9 +176,10 @@ impl ExamplesInstance {
     async fn new<M: Model>(llm: &mut M, current_examples:Vec<(&'static str, &'static str)>,
     unused_examples:Vec<(&'static str, &'static str),>,  temperature:f64
     ,
-    test:&[(&'static str, &'static str)]
+    test:&[(&'static str, &'static str)],
+    metric: &mut impl Metric<String>
     ) -> Self where <<M as kalosm_language::prelude::Model>::SyncModel as kalosm_language::prelude::SyncModel>::Session: Sync+ Send{
-        let current_evaluation = evaluate(&current_examples, test, llm).await;
+        let current_evaluation = evaluate(&current_examples, test, llm, metric).await;
 
         Self {
             current_examples,
@@ -186,7 +191,9 @@ impl ExamplesInstance {
 
     async fn mutate<M: Model>(&mut self,
         test:&[(&'static str, &'static str)],
-    llm: &mut M) where <<M as kalosm_language::prelude::Model>::SyncModel as kalosm_language::prelude::SyncModel>::Session: Sync+ Send{
+    llm: &mut M,
+    metric: &mut impl Metric<String>
+) where <<M as kalosm_language::prelude::Model>::SyncModel as kalosm_language::prelude::SyncModel>::Session: Sync+ Send{
         let action = if self.current_examples.is_empty() {
             2
         } else if self.unused_examples.is_empty() {
@@ -210,7 +217,7 @@ impl ExamplesInstance {
                 let index = random::<usize>() % mutated_examples.len();
                 let removed = mutated_examples.remove(index);
 
-                let new_evaluation = evaluate(&mutated_examples, test, llm).await;
+                let new_evaluation = evaluate(&mutated_examples, test, llm, metric).await;
 
                 if accept_regardless || new_evaluation > self.current_evaluation {
                     self.current_evaluation = new_evaluation;
@@ -225,7 +232,7 @@ impl ExamplesInstance {
 
                 mutated_examples.swap(index1, index2);
 
-                let new_evaluation = evaluate(&mutated_examples, test, llm).await;
+                let new_evaluation = evaluate(&mutated_examples, test, llm, metric).await;
 
                 if accept_regardless || new_evaluation > self.current_evaluation {
                     self.current_evaluation = new_evaluation;
@@ -238,7 +245,7 @@ impl ExamplesInstance {
                 let added = self.unused_examples[index];
                 mutated_examples.push(added);
 
-                let new_evaluation = evaluate(&mutated_examples, test, llm).await;
+                let new_evaluation = evaluate(&mutated_examples, test, llm, metric).await;
 
                 if accept_regardless || new_evaluation > self.current_evaluation {
                     self.current_evaluation = new_evaluation;
@@ -252,7 +259,7 @@ impl ExamplesInstance {
     }
 }
 
-async fn evaluate<M: Model>(examples: &[(&str, &str)], test:&[(&str, &str)], llm: &mut M) -> f64 where <<M as kalosm_language::prelude::Model>::SyncModel as kalosm_language::prelude::SyncModel>::Session: Sync+ Send{
+async fn evaluate<M: Model>(examples: &[(&str, &str)], test:&[(&str, &str)], llm: &mut M, metric: &mut impl Metric<String>) -> f64 where <<M as kalosm_language::prelude::Model>::SyncModel as kalosm_language::prelude::SyncModel>::Session: Sync+ Send{
     let examples_tokens: usize = examples
         .iter()
         .filter_map(|(text, _)| llm.tokenizer().encode(text).ok().map(|x| x.len()))
@@ -275,9 +282,8 @@ async fn evaluate<M: Model>(examples: &[(&str, &str)], test:&[(&str, &str)], llm
         llama_test_cases.push_case(expected.to_string(), actual.clone());
     }
 
-    let mut bert_distance = BertDistance::default();
     let llama_distance = llama_test_cases
-        .evaluate(&mut bert_distance)
+        .evaluate(metric)
         .await
         .normalized();
 
