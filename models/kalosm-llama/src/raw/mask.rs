464f96db3c3377e8bf2846fde097ec907d9511fe
Evan Almloff <evanalmloff@gmail.com>
2024-01-21 21:38:38 -0600

    implement the attention mask

---
+++ models/kalosm-llama/src/raw/mask.rs
@@ -0,0 +1,37 @@
+use candle_core::*;
+use std::collections::HashMap;
+use std::sync::RwLock;
+
+#[derive(Default)]
+pub struct MaskCache {
+    masks: RwLock<HashMap<usize, Tensor>>,
+}
+
+impl MaskCache {
+    pub fn get_mask(&self, seq_len: usize, seqlen_offset: usize) -> Result<Tensor> {
+        let mask = if let Some(mask) = {
+            let masks = self.masks.read().unwrap();
+            masks.get(&seq_len).cloned()
+        } {
+            mask
+        } else {
+            let mask: Vec<_> = (0..seq_len)
+                .flat_map(|i| (0..seq_len).map(move |j| u8::from(j > i)))
+                .collect();
+            let mask = Tensor::from_slice(&mask, (seq_len, seq_len), &Device::Cpu)?;
+            let mut masks = self.masks.write().unwrap();
+            masks.insert(seq_len, mask.clone());
+            mask
+        };
+
+        let mask = if seqlen_offset > 0 {
+			// If this isn't the first token, we need to pad the mask with zeros for the previous tokens.
+            let mask0 = Tensor::zeros((seq_len, seqlen_offset), DType::U8, &Device::Cpu)?;
+            Tensor::cat(&[&mask0, &mask], D::Minus1)?
+        } else {
+            mask
+        };
+
+        Ok(mask)
+    }
+}
--- models/kalosm-llama/src/raw/mod.rs
+++ models/kalosm-llama/src/raw/mod.rs
@@ -3,6 +3,7 @@ use candle_core::Device;
 use candle_transformers::quantized_nn::RmsNorm;
 
 mod attention_layer;
+mod mask;
 mod rope;
 mod silu;
 
@@ -15,135 +16,135 @@ struct LlamaConfig {
 struct Llama {}
 
 impl Llama {
-    pub fn from_ggml(mut ct: ggml_file::Content, gqa: usize) -> Result<Self> {
-        let cpu = &Device::Cpu;
-        let head_dim = (ct.hparams.n_embd / ct.hparams.n_head) as usize;
-        let (cos, sin) = precomput_freqs_cis(head_dim, 10000.)?;
-        let tok_embeddings = ct.remove("tok_embeddings.weight")?;
-        let tok_embeddings = tok_embeddings.dequantize(cpu)?;
-        let norm = RmsNorm::new(ct.remove("norm.weight")?, 1e-5)?;
-        let output = ct.remove("output.weight")?;
-        let mut layers = Vec::with_capacity(ct.hparams.n_layer as usize);
-        for layer_idx in 0..ct.hparams.n_layer {
-            let prefix = format!("layers.{layer_idx}");
-            let attention_wq = ct.remove(&format!("{prefix}.attention.wq.weight"))?;
-            let attention_wk = ct.remove(&format!("{prefix}.attention.wk.weight"))?;
-            let attention_wv = ct.remove(&format!("{prefix}.attention.wv.weight"))?;
-            let attention_wo = ct.remove(&format!("{prefix}.attention.wo.weight"))?;
-            let feed_forward_w1 = ct.remove(&format!("{prefix}.feed_forward.w1.weight"))?;
-            let feed_forward_w2 = ct.remove(&format!("{prefix}.feed_forward.w2.weight"))?;
-            let feed_forward_w3 = ct.remove(&format!("{prefix}.feed_forward.w3.weight"))?;
-            let attention_norm = ct.remove(&format!("{prefix}.attention_norm.weight"))?;
-            let ffn_norm = ct.remove(&format!("{prefix}.ffn_norm.weight"))?;
-            let span_attn = tracing::span!(tracing::Level::TRACE, "attn");
-            let span_rot = tracing::span!(tracing::Level::TRACE, "attn-rot");
-            let span_mlp = tracing::span!(tracing::Level::TRACE, "attn-mlp");
-            layers.push(LayerWeights {
-                attention_wq: QMatMul::from_qtensor(attention_wq)?,
-                attention_wk: QMatMul::from_qtensor(attention_wk)?,
-                attention_wv: QMatMul::from_qtensor(attention_wv)?,
-                attention_wo: QMatMul::from_qtensor(attention_wo)?,
-                attention_norm: RmsNorm::new(attention_norm, 1e-5)?,
-                feed_forward_w1: QMatMul::from_qtensor(feed_forward_w1)?,
-                feed_forward_w2: QMatMul::from_qtensor(feed_forward_w2)?,
-                feed_forward_w3: QMatMul::from_qtensor(feed_forward_w3)?,
-                ffn_norm: RmsNorm::new(ffn_norm, 1e-5)?,
-                n_head: ct.hparams.n_head as usize,
-                n_kv_head: ct.hparams.n_head as usize / gqa,
-                head_dim: (ct.hparams.n_embd / ct.hparams.n_head) as usize,
-                cos: cos.clone(),
-                sin: sin.clone(),
-                span_attn,
-                span_rot,
-                span_mlp,
-            })
-        }
-        let span = tracing::span!(tracing::Level::TRACE, "model");
-        let span_output = tracing::span!(tracing::Level::TRACE, "output");
-        Ok(Self {
-            tok_embeddings: Embedding::new(tok_embeddings, ct.hparams.n_embd as usize),
-            layers,
-            norm,
-            output: QMatMul::from_qtensor(output)?,
-            masks: HashMap::new().into(),
-            span,
-            span_output,
-        })
-    }
+    // pub fn from_ggml(mut ct: ggml_file::Content, gqa: usize) -> Result<Self> {
+    //     let cpu = &Device::Cpu;
+    //     let head_dim = (ct.hparams.n_embd / ct.hparams.n_head) as usize;
+    //     let (cos, sin) = precomput_freqs_cis(head_dim, 10000.)?;
+    //     let tok_embeddings = ct.remove("tok_embeddings.weight")?;
+    //     let tok_embeddings = tok_embeddings.dequantize(cpu)?;
+    //     let norm = RmsNorm::new(ct.remove("norm.weight")?, 1e-5)?;
+    //     let output = ct.remove("output.weight")?;
+    //     let mut layers = Vec::with_capacity(ct.hparams.n_layer as usize);
+    //     for layer_idx in 0..ct.hparams.n_layer {
+    //         let prefix = format!("layers.{layer_idx}");
+    //         let attention_wq = ct.remove(&format!("{prefix}.attention.wq.weight"))?;
+    //         let attention_wk = ct.remove(&format!("{prefix}.attention.wk.weight"))?;
+    //         let attention_wv = ct.remove(&format!("{prefix}.attention.wv.weight"))?;
+    //         let attention_wo = ct.remove(&format!("{prefix}.attention.wo.weight"))?;
+    //         let feed_forward_w1 = ct.remove(&format!("{prefix}.feed_forward.w1.weight"))?;
+    //         let feed_forward_w2 = ct.remove(&format!("{prefix}.feed_forward.w2.weight"))?;
+    //         let feed_forward_w3 = ct.remove(&format!("{prefix}.feed_forward.w3.weight"))?;
+    //         let attention_norm = ct.remove(&format!("{prefix}.attention_norm.weight"))?;
+    //         let ffn_norm = ct.remove(&format!("{prefix}.ffn_norm.weight"))?;
+    //         let span_attn = tracing::span!(tracing::Level::TRACE, "attn");
+    //         let span_rot = tracing::span!(tracing::Level::TRACE, "attn-rot");
+    //         let span_mlp = tracing::span!(tracing::Level::TRACE, "attn-mlp");
+    //         layers.push(LayerWeights {
+    //             attention_wq: QMatMul::from_qtensor(attention_wq)?,
+    //             attention_wk: QMatMul::from_qtensor(attention_wk)?,
+    //             attention_wv: QMatMul::from_qtensor(attention_wv)?,
+    //             attention_wo: QMatMul::from_qtensor(attention_wo)?,
+    //             attention_norm: RmsNorm::new(attention_norm, 1e-5)?,
+    //             feed_forward_w1: QMatMul::from_qtensor(feed_forward_w1)?,
+    //             feed_forward_w2: QMatMul::from_qtensor(feed_forward_w2)?,
+    //             feed_forward_w3: QMatMul::from_qtensor(feed_forward_w3)?,
+    //             ffn_norm: RmsNorm::new(ffn_norm, 1e-5)?,
+    //             n_head: ct.hparams.n_head as usize,
+    //             n_kv_head: ct.hparams.n_head as usize / gqa,
+    //             head_dim: (ct.hparams.n_embd / ct.hparams.n_head) as usize,
+    //             cos: cos.clone(),
+    //             sin: sin.clone(),
+    //             span_attn,
+    //             span_rot,
+    //             span_mlp,
+    //         })
+    //     }
+    //     let span = tracing::span!(tracing::Level::TRACE, "model");
+    //     let span_output = tracing::span!(tracing::Level::TRACE, "output");
+    //     Ok(Self {
+    //         tok_embeddings: Embedding::new(tok_embeddings, ct.hparams.n_embd as usize),
+    //         layers,
+    //         norm,
+    //         output: QMatMul::from_qtensor(output)?,
+    //         masks: HashMap::new().into(),
+    //         span,
+    //         span_output,
+    //     })
+    // }
 
-    pub fn from_gguf<R: std::io::Seek + std::io::Read>(
-        ct: gguf_file::Content,
-        reader: &mut R,
-    ) -> Result<Self> {
-        let cpu = &Device::Cpu;
-        let md_get = |s: &str| match ct.metadata.get(s) {
-            None => candle_core::bail!("cannot find {s} in metadata"),
-            Some(v) => Ok(v),
-        };
+    // pub fn from_gguf<R: std::io::Seek + std::io::Read>(
+    //     ct: gguf_file::Content,
+    //     reader: &mut R,
+    // ) -> Result<Self> {
+    //     let cpu = &Device::Cpu;
+    //     let md_get = |s: &str| match ct.metadata.get(s) {
+    //         None => candle_core::bail!("cannot find {s} in metadata"),
+    //         Some(v) => Ok(v),
+    //     };
 
-        // Parameter extraction from metadata.
-        let head_count = md_get("llama.attention.head_count")?.to_u32()? as usize;
-        let head_count_kv = md_get("llama.attention.head_count_kv")?.to_u32()? as usize;
-        let block_count = md_get("llama.block_count")?.to_u32()? as usize;
-        let embedding_length = md_get("llama.embedding_length")?.to_u32()? as usize;
-        let rope_dim = md_get("llama.rope.dimension_count")?.to_u32()? as usize;
-        // Strangely this value is generally 1e-6 in GGUF file but used to be 1e-5 by default.
-        let rms_norm_eps = md_get("llama.attention.layer_norm_rms_epsilon")?.to_f32()?;
+    //     // Parameter extraction from metadata.
+    //     let head_count = md_get("llama.attention.head_count")?.to_u32()? as usize;
+    //     let head_count_kv = md_get("llama.attention.head_count_kv")?.to_u32()? as usize;
+    //     let block_count = md_get("llama.block_count")?.to_u32()? as usize;
+    //     let embedding_length = md_get("llama.embedding_length")?.to_u32()? as usize;
+    //     let rope_dim = md_get("llama.rope.dimension_count")?.to_u32()? as usize;
+    //     // Strangely this value is generally 1e-6 in GGUF file but used to be 1e-5 by default.
+    //     let rms_norm_eps = md_get("llama.attention.layer_norm_rms_epsilon")?.to_f32()?;
 
-        let rope_freq_base = md_get("llama.rope.freq_base")
-            .and_then(|m| m.to_f32())
-            .unwrap_or(10000f32);
-        let (cos, sin) = precomput_freqs_cis(rope_dim, rope_freq_base)?;
+    //     let rope_freq_base = md_get("llama.rope.freq_base")
+    //         .and_then(|m| m.to_f32())
+    //         .unwrap_or(10000f32);
+    //     let (cos, sin) = precomput_freqs_cis(rope_dim, rope_freq_base)?;
 
-        let tok_embeddings = ct.tensor(reader, "token_embd.weight")?;
-        let tok_embeddings = tok_embeddings.dequantize(cpu)?;
-        let norm = RmsNorm::new(ct.tensor(reader, "output_norm.weight")?, rms_norm_eps)?;
-        let output = ct.tensor(reader, "output.weight")?;
-        let mut layers = Vec::with_capacity(block_count);
-        for layer_idx in 0..block_count {
-            let prefix = format!("blk.{layer_idx}");
-            let attention_wq = ct.tensor(reader, &format!("{prefix}.attn_q.weight"))?;
-            let attention_wk = ct.tensor(reader, &format!("{prefix}.attn_k.weight"))?;
-            let attention_wv = ct.tensor(reader, &format!("{prefix}.attn_v.weight"))?;
-            let attention_wo = ct.tensor(reader, &format!("{prefix}.attn_output.weight"))?;
-            let feed_forward_w1 = ct.tensor(reader, &format!("{prefix}.ffn_gate.weight"))?;
-            let feed_forward_w2 = ct.tensor(reader, &format!("{prefix}.ffn_down.weight"))?;
-            let feed_forward_w3 = ct.tensor(reader, &format!("{prefix}.ffn_up.weight"))?;
-            let attention_norm = ct.tensor(reader, &format!("{prefix}.attn_norm.weight"))?;
-            let ffn_norm = ct.tensor(reader, &format!("{prefix}.ffn_norm.weight"))?;
-            let span_attn = tracing::span!(tracing::Level::TRACE, "attn");
-            let span_rot = tracing::span!(tracing::Level::TRACE, "attn-rot");
-            let span_mlp = tracing::span!(tracing::Level::TRACE, "attn-mlp");
-            layers.push(LayerWeights {
-                attention_wq: QMatMul::from_qtensor(attention_wq)?,
-                attention_wk: QMatMul::from_qtensor(attention_wk)?,
-                attention_wv: QMatMul::from_qtensor(attention_wv)?,
-                attention_wo: QMatMul::from_qtensor(attention_wo)?,
-                attention_norm: RmsNorm::new(attention_norm, rms_norm_eps)?,
-                feed_forward_w1: QMatMul::from_qtensor(feed_forward_w1)?,
-                feed_forward_w2: QMatMul::from_qtensor(feed_forward_w2)?,
-                feed_forward_w3: QMatMul::from_qtensor(feed_forward_w3)?,
-                ffn_norm: RmsNorm::new(ffn_norm, rms_norm_eps)?,
-                n_head: head_count,
-                n_kv_head: head_count_kv,
-                head_dim: embedding_length / head_count,
-                cos: cos.clone(),
-                sin: sin.clone(),
-                span_attn,
-                span_rot,
-                span_mlp,
-            })
-        }
-        let span = tracing::span!(tracing::Level::TRACE, "model");
-        let span_output = tracing::span!(tracing::Level::TRACE, "output");
-        Ok(Self {
-            tok_embeddings: Embedding::new(tok_embeddings, embedding_length),
-            layers,
-            norm,
-            output: QMatMul::from_qtensor(output)?,
-            masks: RwLock::new(HashMap::new()),
-            span,
-            span_output,
-        })
-    }
+    //     let tok_embeddings = ct.tensor(reader, "token_embd.weight")?;
+    //     let tok_embeddings = tok_embeddings.dequantize(cpu)?;
+    //     let norm = RmsNorm::new(ct.tensor(reader, "output_norm.weight")?, rms_norm_eps)?;
+    //     let output = ct.tensor(reader, "output.weight")?;
+    //     let mut layers = Vec::with_capacity(block_count);
+    //     for layer_idx in 0..block_count {
+    //         let prefix = format!("blk.{layer_idx}");
+    //         let attention_wq = ct.tensor(reader, &format!("{prefix}.attn_q.weight"))?;
+    //         let attention_wk = ct.tensor(reader, &format!("{prefix}.attn_k.weight"))?;
+    //         let attention_wv = ct.tensor(reader, &format!("{prefix}.attn_v.weight"))?;
+    //         let attention_wo = ct.tensor(reader, &format!("{prefix}.attn_output.weight"))?;
+    //         let feed_forward_w1 = ct.tensor(reader, &format!("{prefix}.ffn_gate.weight"))?;
+    //         let feed_forward_w2 = ct.tensor(reader, &format!("{prefix}.ffn_down.weight"))?;
+    //         let feed_forward_w3 = ct.tensor(reader, &format!("{prefix}.ffn_up.weight"))?;
+    //         let attention_norm = ct.tensor(reader, &format!("{prefix}.attn_norm.weight"))?;
+    //         let ffn_norm = ct.tensor(reader, &format!("{prefix}.ffn_norm.weight"))?;
+    //         let span_attn = tracing::span!(tracing::Level::TRACE, "attn");
+    //         let span_rot = tracing::span!(tracing::Level::TRACE, "attn-rot");
+    //         let span_mlp = tracing::span!(tracing::Level::TRACE, "attn-mlp");
+    //         layers.push(LayerWeights {
+    //             attention_wq: QMatMul::from_qtensor(attention_wq)?,
+    //             attention_wk: QMatMul::from_qtensor(attention_wk)?,
+    //             attention_wv: QMatMul::from_qtensor(attention_wv)?,
+    //             attention_wo: QMatMul::from_qtensor(attention_wo)?,
+    //             attention_norm: RmsNorm::new(attention_norm, rms_norm_eps)?,
+    //             feed_forward_w1: QMatMul::from_qtensor(feed_forward_w1)?,
+    //             feed_forward_w2: QMatMul::from_qtensor(feed_forward_w2)?,
+    //             feed_forward_w3: QMatMul::from_qtensor(feed_forward_w3)?,
+    //             ffn_norm: RmsNorm::new(ffn_norm, rms_norm_eps)?,
+    //             n_head: head_count,
+    //             n_kv_head: head_count_kv,
+    //             head_dim: embedding_length / head_count,
+    //             cos: cos.clone(),
+    //             sin: sin.clone(),
+    //             span_attn,
+    //             span_rot,
+    //             span_mlp,
+    //         })
+    //     }
+    //     let span = tracing::span!(tracing::Level::TRACE, "model");
+    //     let span_output = tracing::span!(tracing::Level::TRACE, "output");
+    //     Ok(Self {
+    //         tok_embeddings: Embedding::new(tok_embeddings, embedding_length),
+    //         layers,
+    //         norm,
+    //         output: QMatMul::from_qtensor(output)?,
+    //         masks: RwLock::new(HashMap::new()),
+    //         span,
+    //         span_output,
+    //     })
+    // }
 }
