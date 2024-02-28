e4c390d46954685cec68173c2cc16cfb6bfefd6e
Evan Almloff <evanalmloff@gmail.com>
2024-02-28 07:59:39 -0600

    publish a new minor version

--- models/kalosm-llama/src/raw/rope.rs
+++ models/kalosm-llama/src/raw/rope.rs
@@ -72,8 +72,14 @@ impl RopeCache {
             Ok(rope)
         }
 
-        let q = apply_rotary_emb(&self.sin, &self.cos, q, start_pos)?;
-        let k = apply_rotary_emb(&self.sin, &self.cos, k, start_pos)?;
+        let(q,k) = std::thread::scope(|s|{
+            let q = s.spawn(||apply_rotary_emb(&self.sin, &self.cos, q, start_pos));
+            let k = apply_rotary_emb(&self.sin, &self.cos, k, start_pos)?;
+            candle_core::Result::Ok((q.join().map_err(
+                |e|candle_core::Error::Msg(format!("Error in q: {:?}", e))
+            )??, k))
+        })?;
+
         Ok((q, k))
     }
 }
