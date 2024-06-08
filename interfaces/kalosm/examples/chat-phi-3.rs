f48481a1b2e443ee30a9aa7f19cb694fe550ef74
Evan Almloff <evanalmloff@gmail.com>
2024-06-08 09:18:38 -0500

    fix phi3 llama cache

--- interfaces/kalosm/examples/chat-phi-3.rs
+++ interfaces/kalosm/examples/chat-phi-3.rs
@@ -2,11 +2,7 @@ use kalosm::language::*;
 
 #[tokio::main]
 async fn main() {
-    let model = Llama::builder()
-        .with_source(LlamaSource::phi_3_mini_4k_instruct())
-        .build()
-        .await
-        .unwrap();
+    let model = Llama::phi_3().await.unwrap();
     let mut chat = Chat::builder(model)
         .with_system_prompt("You will act like a pirate")
         .build();
