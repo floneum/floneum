3b0547e9c36d75bd96fec846b533420299d7a461
Evan Almloff <evanalmloff@gmail.com>
2024-07-06 12:48:33 -0700

    improve transcribe example docs

--- interfaces/kalosm-sound/examples/transcribe.rs
+++ interfaces/kalosm-sound/examples/transcribe.rs
@@ -3,16 +3,23 @@ use kalosm_streams::text_stream::TextStream;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    // Create a new small whisper model.
+    // Create a new whisper model.
     let model = WhisperBuilder::default()
         .with_source(WhisperSource::DistilLargeV3)
         .build()
         .await?;
+
+    // Stream audio from the microphone
     let mic = MicInput::default();
     let stream = mic.stream().unwrap();
+
+    // Chunk that audio into chunks based on voice activity
     let vad = stream.voice_activity_stream().rechunk_voice_activity();
+
+    // And then transcribe the audio into text
     let text_stream = vad.text(model);
 
+    // Finally, print the text to the console
     text_stream.to_std_out().await.unwrap();
 
     Ok(())
