b403ada6113e683002a522833a9a3ccad2c5cb6f
Evan Almloff <evanalmloff@gmail.com>
2024-07-13 15:11:07 -0700

    Add attributes to modify unit, enum and struct parsing

---
+++ interfaces/kalosm-parse-macro/tests/unit.rs
@@ -0,0 +1,52 @@
+#![allow(unused)]
+
+use kalosm::language::*;
+
+#[derive(Parse, Clone)]
+struct UnitStruct;
+
+#[tokio::test]
+async fn unit_struct() {
+    let model = Llama::builder()
+        .with_source(LlamaSource::tiny_llama_1_1b_chat())
+        .build()
+        .await
+        .unwrap();
+
+    let task = Task::builder("You generate json")
+        .with_constraints(UnitStruct::new_parser())
+        .build();
+
+    let output = task
+        .run("What is the capital of France?", &model)
+        .all_text()
+        .await;
+    println!("{output}");
+
+    assert_eq!(output, "\"UnitStruct\"");
+}
+
+#[derive(Parse, Clone)]
+#[parse(rename = "unit struct")]
+struct RenamedUnit;
+
+#[tokio::test]
+async fn renamed_unit_struct() {
+    let model = Llama::builder()
+        .with_source(LlamaSource::tiny_llama_1_1b_chat())
+        .build()
+        .await
+        .unwrap();
+
+    let task = Task::builder("You generate json")
+        .with_constraints(RenamedUnit::new_parser())
+        .build();
+
+    let output = task
+        .run("What is the capital of France?", &model)
+        .all_text()
+        .await;
+    println!("{output}");
+
+    assert_eq!(output, "\"unit struct\"");
+}
