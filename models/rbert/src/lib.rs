9d8eea4d66c8fd85cdc7beba0cd9ab427c9c52ca
Evan Almloff <evanalmloff@gmail.com>
2024-08-13 22:44:22 -0700

    Add an example to the bert struct

--- models/rbert/src/lib.rs
+++ models/rbert/src/lib.rs
@@ -95,7 +95,7 @@ impl BertBuilder {
     /// Build the model with a loading handler
     ///
     /// ```rust
-    /// use kalosm::sound::*;
+    /// use kalosm::language::*;
     /// # #[tokio::main]
     /// # async fn main() -> Result<(), anyhow::Error> {
     /// // Create a new bert model with a loading handler
@@ -135,7 +135,44 @@ pub enum Pooling {
     CLS,
 }
 
-/// A bert model
+/// A bert embedding model. The main interface for this model is [`EmbedderExt`].
+///
+/// # Example
+/// ```rust, no_run
+/// use kalosm_language_model::Embedder;
+/// use rbert::*;
+///
+/// #[tokio::main]
+/// async fn main() -> anyhow::Result<()> {
+///     let mut bert = Bert::new().await?;
+///     let sentences = [
+///         "Cats are cool",
+///         "The geopolitical situation is dire",
+///         "Pets are great",
+///         "Napoleon was a tyrant",
+///         "Napoleon was a great general",
+///     ];
+///     let embeddings = bert.embed_batch(sentences).await?;
+///     println!("embeddings {:?}", embeddings);
+///
+///     // Find the cosine similarity between the first two sentences
+///     let mut similarities = vec![];
+///     let n_sentences = sentences.len();
+///     for (i, e_i) in embeddings.iter().enumerate() {
+///         for j in (i + 1)..n_sentences {
+///             let e_j = embeddings.get(j).unwrap();
+///             let cosine_similarity = e_j.cosine_similarity(e_i);
+///             similarities.push((cosine_similarity, i, j))
+///         }
+///     }
+///     similarities.sort_by(|u, v| v.0.total_cmp(&u.0));
+///     for &(score, i, j) in similarities.iter() {
+///         println!("score: {score:.2} '{}' '{}'", sentences[i], sentences[j])
+///     }
+///
+///     Ok(())
+/// }
+/// ```
 #[derive(Clone)]
 pub struct Bert {
     embedding_search_prefix: Arc<Option<String>>,
