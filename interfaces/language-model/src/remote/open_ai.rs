de513d0d8825a8105bf81cd3a0356a053ddf5e4b
Evan Almloff <evanalmloff@gmail.com>
2024-09-05 17:02:51 -0500

    Fix openai remote Model implementation (#266)

--- interfaces/language-model/src/remote/open_ai.rs
+++ interfaces/language-model/src/remote/open_ai.rs
@@ -9,50 +9,173 @@ use tokenizers::tokenizer::Tokenizer;
 
 use crate::{Embedder, Embedding, GenerationParameters, ModelBuilder, VectorSpace};
 
-macro_rules! openai_model {
+/// A model that uses OpenAI's API.
+pub struct RemoteOpenAICompatibleModel {
+    model: String,
+    client: Client<async_openai::config::OpenAIConfig>,
+}
+
+/// A builder for any remote OpenAI compatible model.
+#[derive(Debug, Default)]
+pub struct RemoteOpenAICompatibleModelBuilder<const WITH_NAME: bool> {
+    model: Option<String>,
+    config: async_openai::config::OpenAIConfig,
+}
+
+impl RemoteOpenAICompatibleModelBuilder<false> {
+    /// Creates a new builder
+    pub fn new() -> Self {
+        Self {
+            model: None,
+            config: Default::default(),
+        }
+    }
+
+    /// Set the name of the model to use.
+    pub fn with_model(self, model: impl ToString) -> RemoteOpenAICompatibleModelBuilder<true> {
+        RemoteOpenAICompatibleModelBuilder {
+            model: Some(model.to_string()),
+            config: self.config,
+        }
+    }
+}
+
+impl<const WITH_NAME: bool> RemoteOpenAICompatibleModelBuilder<WITH_NAME> {
+    /// Sets the API key for the builder.
+    pub fn with_api_key(mut self, api_key: &str) -> Self {
+        self.config = self.config.with_api_key(api_key);
+        self
+    }
+
+    /// Set the base URL of the API.
+    pub fn with_base_url(mut self, base_url: &str) -> Self {
+        self.config = self.config.with_api_base(base_url);
+        self
+    }
+
+    /// Set the organization ID for the builder.
+    pub fn with_organization_id(mut self, organization_id: &str) -> Self {
+        self.config = self.config.with_org_id(organization_id);
+        self
+    }
+}
+
+impl RemoteOpenAICompatibleModelBuilder<true> {
+    /// Build the model.
+    pub fn build(self) -> RemoteOpenAICompatibleModel {
+        RemoteOpenAICompatibleModel {
+            model: self.model.unwrap(),
+            client: Client::with_config(self.config),
+        }
+    }
+}
+
+impl RemoteOpenAICompatibleModel {
+    /// Creates a new builder
+    pub fn builder() -> RemoteOpenAICompatibleModelBuilder<false> {
+        RemoteOpenAICompatibleModelBuilder::new()
+    }
+}
+
+#[async_trait::async_trait]
+impl crate::model::Model for RemoteOpenAICompatibleModel {
+    type TextStream = ChannelTextStream;
+    type SyncModel = crate::SyncModelNotSupported;
+
+    fn tokenizer(&self) -> Arc<Tokenizer> {
+        panic!("OpenAI does not expose tokenization")
+    }
+
+    async fn stream_text_inner(
+        &self,
+        prompt: &str,
+        generation_parameters: GenerationParameters,
+    ) -> anyhow::Result<Self::TextStream> {
+        let mut builder = CreateCompletionRequestArgs::default();
+        builder
+            .model(&self.model)
+            .n(1)
+            .prompt(prompt)
+            .stream(true)
+            .frequency_penalty(generation_parameters.repetition_penalty)
+            .temperature(generation_parameters.temperature)
+            .max_tokens(generation_parameters.max_length as u16);
+        if let Some(stop_on) = generation_parameters.stop_on {
+            builder.stop(stop_on);
+        }
+        let request = builder.build()?;
+
+        let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+
+        let mut stream = self.client.completions().create_stream(request).await?;
+
+        tokio::spawn(async move {
+            while let Some(response) = stream.next().await {
+                match response {
+                    Ok(response) => {
+                        let text = response.choices[0].text.clone();
+                        if tx.send(text).is_err() {
+                            break;
+                        }
+                    }
+                    Err(e) => {
+                        log::error!("Error in OpenAI stream: {}", e);
+                        break;
+                    }
+                }
+            }
+
+            Ok::<(), anyhow::Error>(())
+        });
+
+        Ok(rx.into())
+    }
+}
+
+macro_rules! openai_completion_model {
     ($ty: ident, $tybuilder: ident, $model: literal) => {
         /// A model that uses OpenAI's API.
         pub struct $ty {
-            client: Client<async_openai::config::OpenAIConfig>,
+            inner: RemoteOpenAICompatibleModel,
         }
 
         /// A builder for
         #[doc = $model]
         #[derive(Debug, Default)]
         pub struct $tybuilder {
-            config: async_openai::config::OpenAIConfig,
+            inner: RemoteOpenAICompatibleModelBuilder<true>,
         }
 
         impl $tybuilder {
             /// Creates a new builder
             pub fn new() -> Self {
                 Self {
-                    config: Default::default(),
+                    inner: RemoteOpenAICompatibleModelBuilder::new().with_model($model),
                 }
             }
 
             /// Sets the API key for the builder.
             pub fn with_api_key(mut self, api_key: &str) -> Self {
-                self.config = self.config.with_api_key(api_key);
+                self.inner = self.inner.with_api_key(api_key);
                 self
             }
 
             /// Set the base URL of the API.
             pub fn with_base_url(mut self, base_url: &str) -> Self {
-                self.config = self.config.with_api_base(base_url);
+                self.inner = self.inner.with_base_url(base_url);
                 self
             }
 
             /// Set the organization ID for the builder.
             pub fn with_organization_id(mut self, organization_id: &str) -> Self {
-                self.config = self.config.with_org_id(organization_id);
+                self.inner = self.inner.with_organization_id(organization_id);
                 self
             }
 
             /// Build the model.
             pub fn build(self) -> $ty {
                 $ty {
-                    client: Client::with_config(self.config),
+                    inner: self.inner.build(),
                 }
             }
         }
@@ -78,8 +201,9 @@ macro_rules! openai_model {
                 self,
                 _: impl FnMut(ModelLoadingProgress) + Send + Sync + 'static,
             ) -> anyhow::Result<$ty> {
-                let client = Client::new();
-                Ok($ty { client })
+                Ok($ty {
+                    inner: self.inner.build(),
+                })
             }
 
             fn requires_download(&self) -> bool {
@@ -101,54 +225,20 @@ macro_rules! openai_model {
                 prompt: &str,
                 generation_parameters: GenerationParameters,
             ) -> anyhow::Result<Self::TextStream> {
-                let request = CreateCompletionRequestArgs::default()
-                    .model($model)
-                    .n(1)
-                    .prompt(prompt)
-                    .stream(true)
-                    .frequency_penalty(generation_parameters.repetition_penalty)
-                    .temperature(generation_parameters.temperature)
-                    .stop(
-                        generation_parameters
-                            .stop_on
-                            .iter()
-                            .cloned()
-                            .collect::<Vec<String>>(),
-                    )
-                    .max_tokens(generation_parameters.max_length as u16)
-                    .build()?;
-
-                let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
-
-                let mut stream = self.client.completions().create_stream(request).await?;
-
-                tokio::spawn(async move {
-                    while let Some(response) = stream.next().await {
-                        match response {
-                            Ok(response) => {
-                                let text = response.choices[0].text.clone();
-                                if tx.send(text).is_err() {
-                                    break;
-                                }
-                            }
-                            Err(e) => {
-                                log::error!("Error in OpenAI stream: {}", e);
-                                break;
-                            }
-                        }
-                    }
-
-                    Ok::<(), anyhow::Error>(())
-                });
-
-                Ok(rx.into())
+                self.inner
+                    .stream_text_inner(prompt, generation_parameters)
+                    .await
             }
         }
     };
 }
 
-openai_model!(Gpt3_5, Gpt3_5Builder, "gpt-3.5-turbo");
-openai_model!(Gpt4, Gpt4Builder, "text-davinci-003");
+openai_completion_model!(Gpt3_5, Gpt3_5Builder, "gpt-3.5-turbo-instruct");
+// The rest of the openai models only support the chat API which currently isn't supported for remote models in kalosm
+// openai_chat_model!(Gpt4, Gpt4Builder, "gpt-4");
+// openai_chat_model!(Gpt4Turbo, Gpt4TurboBuilder, "gpt-4-turbo");
+// openai_chat_model!(Gpt4O, Gpt4OBuilder, "gpt-4o");
+// openai_chat_model!(Gpt4Mini, Gpt4MiniBuilder, "gpt-4o-mini");
 
 /// An embedder that uses OpenAI's API for the Ada embedding model.
 #[derive(Debug)]
