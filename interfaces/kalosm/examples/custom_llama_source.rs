5e6ad9d996713015ff7ad6cf5c18d47621444a95
Evan Almloff <evanalmloff@gmail.com>
2024-12-28 10:06:17 -0600

    simplify custom llama source example

--- interfaces/kalosm/examples/custom_llama_source.rs
+++ interfaces/kalosm/examples/custom_llama_source.rs
@@ -1,5 +1,3 @@
-use std::io::Write;
-
 use kalosm::language::*;
 
 #[tokio::main]
@@ -27,11 +25,7 @@ async fn main() {
     let prompt = "The following is a 300 word essay about why the capital of France is Paris:";
     print!("{}", prompt);
 
-    let stream = llm.stream_text(prompt).with_max_length(300).await.unwrap();
+    let mut stream = llm.stream_text(prompt).with_max_length(300).await.unwrap();
 
-    let mut sentences = stream.words();
-    while let Some(text) = sentences.next().await {
-        print!("{}", text);
-        std::io::stdout().flush().unwrap();
-    }
+    stream.to_std_out().await.unwrap();
 }
