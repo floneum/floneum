55ff33ecdad171aefa8f207ee75383fd323ae9d2
Evan Almloff <evanalmloff@gmail.com>
2025-01-27 17:13:29 -0600

    fix formatting

--- interfaces/kalosm/examples/task-r1.rs
+++ interfaces/kalosm/examples/task-r1.rs
@@ -15,41 +15,18 @@ struct Account {
 
 #[tokio::main]
 async fn main() {
-    let llm = loop {
+    let source = loop {
         let input =
             prompt_input("Model (choose from qwen-1.5b, qwen-7b, qwen-14b, llama-8b): ").unwrap();
         match input.as_str() {
-            "qwen-1.5b" => {
-                break Llama::builder()
-                    .with_source(LlamaSource::deepseek_r1_distill_qwen_1_5b())
-                    .build()
-                    .await
-                    .unwrap()
-            }
-            "qwen-7b" => {
-                break Llama::builder()
-                    .with_source(LlamaSource::deepseek_r1_distill_qwen_7b())
-                    .build()
-                    .await
-                    .unwrap()
-            }
-            "qwen-14b" => {
-                break Llama::builder()
-                    .with_source(LlamaSource::deepseek_r1_distill_qwen_14b())
-                    .build()
-                    .await
-                    .unwrap()
-            }
-            "llama-8b" => {
-                break Llama::builder()
-                    .with_source(LlamaSource::deepseek_r1_distill_llama_8b())
-                    .build()
-                    .await
-                    .unwrap()
-            }
+            "qwen-1.5b" => break LlamaSource::deepseek_r1_distill_qwen_1_5b(),
+            "qwen-7b" => break LlamaSource::deepseek_r1_distill_qwen_7b(),
+            "qwen-14b" => break LlamaSource::deepseek_r1_distill_qwen_14b(),
+            "llama-8b" => break LlamaSource::deepseek_r1_distill_llama_8b(),
             _ => println!("Invalid model... try again"),
         }
     };
+    let llm = Llama::builder().with_source(source).build().await.unwrap();
 
     // Create a task that generates a list of accounts
     let task = llm.task("You generate accounts based on a description of the account holder in this format { \"summary\": \"A summary of the account holder\", \"name\": \"The name of the account holder\", \"age\": numericAgeInYears }");
