0400aaa913b3b05ee72322d38efd783711d51d02
Evan Almloff <evanalmloff@gmail.com>
2025-02-09 13:03:50 -0600

    temporarily disable python plugin

--- floneum/plugins/python/src/lib.rs
+++ floneum/plugins/python/src/lib.rs
@@ -1,5 +1,5 @@
 use floneum_rust::*;
-use rustpython_vm::{builtins, stdlib::get_module_inits, Interpreter};
+// use rustpython_vm::{builtins, stdlib::get_module_inits, Interpreter};
 
 #[export_plugin]
 /// Runs a python script. Returns the value in the last line
@@ -14,18 +14,19 @@ use rustpython_vm::{builtins, stdlib::get_module_inits, Interpreter};
 /// ]
 pub fn run_python(
     /// The python script to run
-    source: String,
+    _source: String,
 ) -> String {
-    Interpreter::with_init(Default::default(), |vm| {
-        vm.add_native_modules(get_module_inits());
-    })
-    .enter(|vm| {
-        let scope = vm.new_scope_with_builtins();
+    // Interpreter::with_init(Default::default(), |vm| {
+    //     vm.add_native_modules(get_module_inits());
+    // })
+    // .enter(|vm| {
+    //     let scope = vm.new_scope_with_builtins();
 
-        vm.run_block_expr(scope, &source)
-            .unwrap()
-            .downcast::<builtins::PyStr>()
-            .map(|s| s.to_string())
-            .unwrap_or_default()
-    })
+    //     vm.run_block_expr(scope, &source)
+    //         .unwrap()
+    //         .downcast::<builtins::PyStr>()
+    //         .map(|s| s.to_string())
+    //         .unwrap_or_default()
+    // })
+    todo!()
 }
