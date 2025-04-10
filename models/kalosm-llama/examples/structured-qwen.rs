85b5fb6b8f0682152cac4caf62756b58597470b8
Evan Almloff <evanalmloff@gmail.com>
2025-04-10 18:10:43 -0500

    add qwen example

---
+++ models/kalosm-llama/examples/structured-qwen.rs
@@ -0,0 +1,88 @@
+#![allow(unused)]
+use kalosm::language::*;
+use std::{io::Write, sync::Arc};
+
+#[tokio::main]
+async fn main() {
+    let llm = Llama::builder()
+        .with_source(LlamaSource::qwen_2_5_0_5b_instruct())
+        .build()
+        .await
+        .unwrap();
+    let prompt = "Create a list of pets";
+
+    #[derive(Debug, Clone, Parse)]
+    struct Pet {
+        #[parse(len = 1..=10)]
+        name: String,
+        #[parse(len = 1..=20)]
+        description: String,
+        #[parse(len = 1..=10)]
+        color: String,
+        size: Size,
+        diet: Diet,
+    }
+
+    #[derive(Debug, Clone, Parse)]
+    enum Diet {
+        #[parse(rename = "carnivore")]
+        Carnivore,
+        #[parse(rename = "herbivore")]
+        Herbivore,
+        #[parse(rename = "omnivore")]
+        Omnivore,
+    }
+
+    #[derive(Debug, Clone, Parse)]
+    enum Size {
+        #[parse(rename = "small")]
+        Small,
+        #[parse(rename = "medium")]
+        Medium,
+        #[parse(rename = "large")]
+        Large,
+    }
+
+    println!("# with constraints");
+
+    // let sampler = GenerationParameters::new().with_seed(0);
+    let sampler = GenerationParameters::new();
+
+    let task = llm
+        .task("You generate realistic JSON placeholders")
+        .with_constraints(Arc::new(<[Pet; 4] as Parse>::new_parser()));
+    let stream = task.run(prompt).with_sampler(sampler.clone());
+
+    time_stream(stream).await;
+
+    println!("\n\n# without constraints");
+
+    let task = llm.task("You generate realistic JSON placeholders");
+    let stream = task(prompt).with_sampler(sampler);
+
+    time_stream(stream).await;
+}
+
+async fn time_stream(mut stream: impl TextStream + Unpin) {
+    let start_time = std::time::Instant::now();
+    let mut tokens = 0;
+    let mut string_length = 0;
+    let mut all_text = String::new();
+    while let Some(token) = stream.next().await {
+        tokens += 1;
+        string_length += token.len();
+        all_text.push_str(&token);
+        // print!("{token}");
+        // std::io::stdout().flush().unwrap();
+    }
+    let elapsed = start_time.elapsed();
+    println!("\n\n{all_text}");
+    println!(
+        "\n\nGenerated {} tokens ({} characters) in {:?}",
+        tokens, string_length, elapsed
+    );
+    println!(
+        "Tokens per second: {:.2}",
+        tokens as f64 / elapsed.as_secs_f64()
+    );
+}
