85716d5fe533afbcd0ad1ba78c8c854a3ef94cec
Evan Almloff <evanalmloff@gmail.com>
2025-04-17 16:50:29 -0500

    add structured html example

---
+++ models/kalosm-llama/examples/structured-html.rs
@@ -0,0 +1,55 @@
+#![allow(unused)]
+use kalosm::language::*;
+use kalosm_llama::{EvaluationTrie, LlamaModel};
+use std::{io::Write, sync::Arc};
+use html_parser::Element;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
+    // let sampler = GenerationParameters::new().with_seed(0);
+    let sampler = GenerationParameters::new();
+
+    let mut llm = LlamaModel::from_builder(
+        Llama::builder().with_source(LlamaSource::qwen_2_5_0_5b_instruct()),
+        ModelLoadingProgress::multi_bar_loading_indicator(),
+    )
+    .await
+    .unwrap();
+
+    tokio::task::spawn_blocking(move || {
+       let mut trie = EvaluationTrie::new();
+        let mut last_entropy = 0.0;
+        let task = prompt_input("Enter a task: ").unwrap();
+        for generation in 0.. {
+           let mut session = llm.new_session();
+           let parser = Element::new_parser();
+
+           let output = llm.generate_structured_with_trie(
+               &mut session,
+               &format!("<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.<|im_end|>\n<|im_start|>user\n{task}<|im_end|>\n<|im_start|>assistant\n"),
+               sampler.clone(),
+               parser,
+               |token| {
+                   print!("{}", token);
+                   std::io::stdout().flush().unwrap();
+                   Ok(())
+               },
+               &mut trie,
+           ).unwrap();
+           println!("\n\n");
+
+            println!("generation {generation}:\n{output:?}");
+            let mut shannon_entropy = trie.shannon_entropy();
+            let entropy_diff = last_entropy - shannon_entropy;
+            println!("entropy diff: {entropy_diff}");
+            if entropy_diff.abs() < 0.00001 {
+                println!("looks like entropy is converging, stopping generation");
+                break;
+            }
+            println!("shannon entropy: {shannon_entropy}");
+            last_entropy = shannon_entropy;
+       }
+   }).await.unwrap();
+}
