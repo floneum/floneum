2e24f4794d06d076a2b43adba42e836dc01edc8e
Evan Almloff <evanalmloff@gmail.com>
2025-04-17 08:44:58 -0500

    fix softmax test

--- fusor-ml/core/src/composite/softmax.rs
+++ fusor-ml/core/src/composite/softmax.rs
@@ -11,7 +11,7 @@ where
         let normalized = self - &max.broadcast(size);
         let exp = normalized.exp();
         let sum = exp.sum(dim);
-        self / &sum.broadcast(size)
+        exp / sum.broadcast(size)
     }
 
     pub fn softmax_last_dim(&self) -> Self {
@@ -27,18 +27,18 @@ async fn test_softmax() {
     let device = Device::new().await.unwrap();
 
     let data = [1f32, -2., -3., 4., 5., -6.];
-    let exp: [f32; 6] = std::array::from_fn(|i| data[i].exp());
+    let max = data.iter().cloned().fold(f32::NEG_INFINITY, f32::max);
+    let diff: [f32; 6] = std::array::from_fn(|i| data[i] - max);
+    let exp: [f32; 6] = std::array::from_fn(|i| diff[i].exp());
     let sum = exp.iter().sum::<f32>();
-    println!("{:?}", sum);
     let softmax_array: [f32; 6] = std::array::from_fn(|i| exp[i] / sum);
 
+    println!("{:?}", softmax_array);
+
     let tensor = Tensor::new(&device, &data);
-
-    let tensor = tensor.softmax(0);
-
+    let tensor = tensor.softmax(0).await;
     let output = tensor.as_slice().await.unwrap();
     println!("{:?}", output);
-    println!("{:?}", softmax_array);
     assert!((output[[0]] - softmax_array[0]).abs() < 0.001);
     assert!((output[[1]] - softmax_array[1]).abs() < 0.001);
     assert!((output[[2]] - softmax_array[2]).abs() < 0.001);
