42c314feb78745c10c580ea2f021d0902e86bbff
Evan Almloff <evanalmloff@gmail.com>
2025-04-21 12:52:22 -0500

    plug in qwen

--- models/kalosm-llama/examples/sygus.rs
+++ models/kalosm-llama/examples/sygus.rs
@@ -53,9 +53,18 @@
 
 // (check-synth)
 
-use std::{collections::HashMap, sync::{Arc, RwLock}};
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
 
-use kalosm_sample::{AnyOfSchema, ArcParser, CreateParserState, LazyParser, LiteralParser, Parse, Parser, ParserExt, SendCreateParserState, SequenceParser};
+use kalosm::language::*;
+use kalosm_llama::{EvaluationTrie, LlamaModel};
+use kalosm_sample::{
+    ArcParser, CreateParserState, LazyParser, LiteralParser, Parse, Parser, ParserExt,
+    SendCreateParserState, SequenceParser,
+};
+use std::io::Write;
 
 #[derive(Clone, Debug)]
 enum Expression {
@@ -85,7 +94,15 @@ impl FunctionMap {
         FunctionMap {
             functions_with_return_type: TypeSchema::ALL
                 .iter()
-                .map(|ty| (*ty, Arc::new(RwLock::new(vec![ty.literal_parser().map_output(Expression::Literal).boxed()]))))
+                .map(|ty| {
+                    (
+                        *ty,
+                        Arc::new(RwLock::new(vec![ty
+                            .literal_parser()
+                            .map_output(Expression::Literal)
+                            .boxed()])),
+                    )
+                })
                 .collect(),
         }
     }
@@ -95,14 +112,13 @@ impl FunctionMap {
         LazyParser::new(move || {
             let parser_list = parser_list.read().unwrap();
             let mut parser_list_iter = parser_list.iter();
-            let mut parser = parser_list_iter
-                .next()
-                .unwrap().clone();
+            let mut parser = parser_list_iter.next().unwrap().clone();
             for p in parser_list_iter {
                 parser = parser.or(p.clone()).boxed();
             }
             parser
-        }).boxed()
+        })
+        .boxed()
     }
 }
 
@@ -121,17 +137,22 @@ impl FunctionSchema {
         }
     }
 
-    fn arg_parser(&self, functions: &FunctionMap) -> impl SendCreateParserState<Output = Vec<Expression>> {
+    fn arg_parser(
+        &self,
+        functions: &FunctionMap,
+    ) -> impl SendCreateParserState<Output = Vec<Expression>> {
         let parsers: Vec<_> = self.args.iter().map(|arg| functions.parser(*arg)).collect();
 
         let mut parser = LiteralParser::new("").map_output(|_| Vec::new()).boxed();
         for p in parsers {
-            parser = parser.then_literal(" ").then(p).map_output(
-                |(mut args, arg)| {
+            parser = parser
+                .then_literal(" ")
+                .then(p)
+                .map_output(|(mut args, arg)| {
                     args.push(arg);
                     args
-                }
-            ).boxed();
+                })
+                .boxed();
         }
         parser
     }
@@ -139,15 +160,11 @@ impl FunctionSchema {
     fn parser(&self, functions: &FunctionMap) -> impl SendCreateParserState<Output = Call> {
         let name = self.name.clone();
         LiteralParser::new(format!("({}", self.name))
-            .ignore_output_then(
-                    self.arg_parser(functions)
-            )
+            .ignore_output_then(self.arg_parser(functions))
             .then_literal(")")
-            .map_output(move |args| {
-                Call {
-                    name: name.clone(),
-                    args,
-                }
+            .map_output(move |args| Call {
+                name: name.clone(),
+                args,
             })
     }
 }
@@ -164,38 +181,108 @@ impl TypeSchema {
 
     fn literal_parser(&self) -> impl SendCreateParserState<Output = Literal> {
         match self {
-            TypeSchema::Int => i32::new_parser().map_output(|value| Literal::Int(value)).boxed(),
-            TypeSchema::Bool => bool::new_parser().map_output(|value| Literal::Bool(value)).boxed(),
-            TypeSchema::String => String::new_parser().map_output(|value| Literal::String(value)).boxed(),
+            TypeSchema::Int => i32::new_parser()
+                .map_output(|value| Literal::Int(value))
+                .boxed(),
+            TypeSchema::Bool => bool::new_parser()
+                .map_output(|value| Literal::Bool(value))
+                .boxed(),
+            TypeSchema::String => String::new_parser()
+                .map_output(|value| Literal::String(value))
+                .boxed(),
         }
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
     let mut function_map = FunctionMap::new();
 
     let function_schemas = vec![
-        FunctionSchema::new("max2".to_string(), vec![TypeSchema::Int, TypeSchema::Int], TypeSchema::Int),
-        FunctionSchema::new("str.len".to_string(), vec![TypeSchema::String], TypeSchema::Int),
-        FunctionSchema::new("f".to_string(), vec![TypeSchema::String, TypeSchema::String], TypeSchema::String),
+        FunctionSchema::new(
+            "max2".to_string(),
+            vec![TypeSchema::Int, TypeSchema::Int],
+            TypeSchema::Int,
+        ),
+        FunctionSchema::new(
+            "str.len".to_string(),
+            vec![TypeSchema::String],
+            TypeSchema::Int,
+        ),
     ];
 
     for function_schema in function_schemas {
         let parser = function_schema.parser(&function_map);
-        function_map.functions_with_return_type
+        function_map
+            .functions_with_return_type
             .entry(function_schema.return_type)
             .or_default()
             .write()
             .unwrap()
-            .push(parser
-                .map_output(Expression::Call)
-                .boxed());
+            .push(parser.map_output(Expression::Call).boxed());
     }
 
     let parser = function_map.parser(TypeSchema::Int);
-    let result = parser.parse(&parser.create_parser_state(), b"(max2 1 2)").unwrap();
+    let result = parser
+        .parse(&parser.create_parser_state(), b"(max2 1 2)")
+        .unwrap();
     println!("{:?}", result);
 
-    let another_result = parser.parse(&parser.create_parser_state(), b"(str.len \"hello\")").unwrap();
+    let result = parser
+        .parse(
+            &parser.create_parser_state(),
+            b"(max2 (str.len \"hello\") (max2 1 2))",
+        )
+        .unwrap();
+    println!("{:?}", result);
+
+    let another_result = parser
+        .parse(&parser.create_parser_state(), b"(str.len \"hello\")")
+        .unwrap();
     println!("{:?}", another_result);
+
+    let sampler = GenerationParameters::new();
+
+    let mut llm = LlamaModel::from_builder(
+        Llama::builder().with_source(LlamaSource::qwen_2_5_0_5b_instruct()),
+        ModelLoadingProgress::multi_bar_loading_indicator(),
+    )
+    .await
+    .unwrap();
+
+    tokio::task::spawn_blocking(move || {
+       let mut trie = EvaluationTrie::new();
+         let mut last_entropy = 0.0;
+         let task = r#"Write code for the maximum length of the two strings "hello" and "world". Functions available are: (max2 int int), and (str.len string)."#;
+       for generation in 0.. {
+           let mut session = llm.new_session();
+
+           let output = llm.generate_structured_with_trie(
+               &mut session,
+               &format!("<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.<|im_end|>\n<|im_start|>user\n{task}<|im_end|>\n<|im_start|>assistant\nHere is the code:\n"),
+               sampler.clone(),
+               parser.clone(),
+               |token| {
+                   print!("{}", token);
+                   std::io::stdout().flush().unwrap();
+                   Ok(())
+               },
+               &mut trie,
+           ).unwrap();
+           println!("\n\n");
+
+            println!("generation {generation}:\n{output:?}");
+            let shannon_entropy = trie.shannon_entropy();
+            let entropy_diff = last_entropy - shannon_entropy;
+            println!("entropy diff: {entropy_diff}");
+            if entropy_diff.abs() < 0.00001 {
+                println!("looks like entropy is converging, stopping generation");
+                break;
+            }
+            println!("shannon entropy: {shannon_entropy}");
+            last_entropy = shannon_entropy;
+       }
+   }).await.unwrap();
 }
