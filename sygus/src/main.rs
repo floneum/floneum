d619b3b6ab87b35b219d93c1ebcc116a023a0081
Evan Almloff <evanalmloff@gmail.com>
2025-04-25 10:24:50 -0500

    derive term parser

--- sygus/src/main.rs
+++ sygus/src/main.rs
@@ -1,5 +1,18 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, OnceLock, RwLock},
+};
+
+use kalosm::language::*;
+use kalosm_llama::{EvaluationTrie, LlamaModel};
+use kalosm_sample::{
+    ArcParser, CreateParserState, LazyParser, LiteralParser, Parse, Parser, ParserExt,
+    SendCreateParserState, SequenceParser,
+};
+use std::io::Write;
+
 use nom::{
-    IResult, Parser,
+    IResult, Parser as _,
     branch::alt,
     bytes::complete::{is_not, tag, take_while1},
     character::complete::multispace1,
@@ -137,32 +150,19 @@ fn to_command(expr: SExpr) -> Option<Command> {
                         })
                         .collect()
                 } else {
+                    println!("args was none");
                     return None;
                 };
                 let ret_ty = match &rest[2] {
                     SExpr::Atom(t) => t.clone(),
-                    _ => return None,
+                    _ => {
+                        println!("ret was none");
+                        return None;
+                    }
                 };
-                // non-terms
-                let non_terms = if let SExpr::List(nt) = &rest[3] {
-                    nt.iter()
-                        .filter_map(|p| {
-                            if let SExpr::List(v) = p {
-                                if let [SExpr::Atom(n), SExpr::Atom(t)] = &v[..] {
-                                    Some((n.clone(), t.clone()))
-                                } else {
-                                    None
-                                }
-                            } else {
-                                None
-                            }
-                        })
-                        .collect()
-                } else {
-                    return None;
-                };
+                let non_terms = Vec::new();
                 // grammar rules
-                let grammar = if let SExpr::List(gr) = &rest[4] {
+                let grammar = if let Some(SExpr::List(gr)) = &rest.get(3) {
                     gr.iter()
                         .filter_map(|p| {
                             if let SExpr::List(v) = p {
@@ -178,13 +178,13 @@ fn to_command(expr: SExpr) -> Option<Command> {
                         })
                         .collect()
                 } else {
-                    return None;
+                    Vec::new()
                 };
                 Some(Command::SynthFun(SynthFun {
                     name,
                     args,
                     ret_ty,
-                    non_terms,
+                    non_terminals: non_terms,
                     grammar,
                 }))
             }
@@ -208,12 +208,69 @@ pub enum SExpr {
 
 /// A synthesized function
 #[derive(Debug, Clone, PartialEq)]
-struct SynthFun {
-    name: String,
-    args: Vec<(String, String)>,
-    ret_ty: String,
-    non_terms: Vec<(String, String)>,
-    grammar: Vec<(String, String, Vec<SExpr>)>,
+pub struct SynthFun {
+    pub name: String,
+    pub args: Vec<(String, String)>,
+    pub ret_ty: String,
+    pub non_terminals: Vec<(String, String)>,
+    pub grammar: Vec<(String, String, Vec<SExpr>)>,
+}
+
+impl SynthFun {
+    fn parser(&self) -> ArcParser<SExpr> {
+        let map = TermMap::new(self.grammar.iter().map(|(n, _, _)| n.clone()));
+
+        for (name, _, rules) in &self.grammar {
+            let parser = parse_any_of_non_empty_list(rules.iter().map(|term| map.parser(term)));
+            if map.functions_with_return_type[name].set(parser).is_err() {
+                panic!("Multiple grammars for the same term")
+            }
+        }
+
+        map.parser_for_term("Start").unwrap()
+    }
+}
+
+struct TermMap {
+    functions_with_return_type: HashMap<String, Arc<OnceLock<ArcParser<SExpr>>>>,
+}
+
+impl TermMap {
+    fn new(terms: impl IntoIterator<Item = String>) -> Self {
+        Self {
+            functions_with_return_type: terms
+                .into_iter()
+                .map(|term| (term, Arc::new(OnceLock::new())))
+                .collect(),
+        }
+    }
+
+    fn parser(&self, expr: &SExpr) -> ArcParser<SExpr> {
+        match expr {
+            SExpr::Atom(atom) => self.parser_for_term(&*atom).unwrap_or_else(|| {
+                let atom = atom.clone();
+                LiteralParser::new(atom.clone())
+                    .map_output(move |_| SExpr::Atom(atom.clone()))
+                    .boxed()
+            }),
+            SExpr::List(sexprs) => LiteralParser::new("(")
+                .ignore_output_then(
+                    parse_non_empty_list_sequentially(sexprs.iter().map(|term| self.parser(term)))
+                        .map_output(SExpr::List),
+                )
+                .then_literal(")")
+                .boxed(),
+        }
+    }
+
+    fn parser_for_term(&self, term: &str) -> Option<ArcParser<SExpr>> {
+        self.functions_with_return_type
+            .get(term)
+            .map(|parser_list| {
+                let parser_list = parser_list.clone();
+                LazyParser::new(move || parser_list.get().unwrap().clone()).boxed()
+            })
+    }
 }
 
 /// High-level SyGuS commands
@@ -243,25 +300,96 @@ pub fn parse_sygus(input: &str) -> Result<Vec<Command>, String> {
     }
 }
 
-fn main() {
-    let src = r#"
-      ;; The background theory is linear integer arithmetic
-      (set-logic LIA)
-      ;; Name and signature of the function to be synthesized
-      (synth-fun max2 ((x Int) (y Int)) Int
-          ((I Int) (B Bool))
-          ((I Int (x y 0 1 (+ I I) (- I I) (ite B I I)))
-           (B Bool ((and B B) (or B B) (not B) (= I I) (<= I I) (>= I I))))
-      )
-      (declare-var x Int)
-      (declare-var y Int)
-      (constraint (>= (max2 x y) x))
-      (constraint (>= (max2 x y) y))
-      (constraint (or (= x (max2 x y)) (= y (max2 x y))))
-      (check-synth)
-    "#;
-    match parse_sygus(src) {
-        Ok(cmds) => println!("{:#?}", cmds),
-        Err(e) => eprintln!("Error: {}", e),
+fn parse_non_empty_list_sequentially<P: SendCreateParserState + 'static>(
+    parsers: impl IntoIterator<Item = P>,
+) -> ArcParser<Vec<P::Output>> {
+    let mut iter = parsers.into_iter();
+    let mut parser = iter.next().unwrap().map_output(|out| vec![out]).boxed();
+    for next in iter {
+        parser = parser
+            .then_literal(" ")
+            .then(next)
+            .map_output(|(mut list, new)| {
+                list.push(new);
+                list
+            })
+            .boxed();
     }
+    parser
+}
+
+fn parse_any_of_non_empty_list<P: SendCreateParserState + 'static>(
+    parsers: impl IntoIterator<Item = P>,
+) -> ArcParser<P::Output> {
+    let mut iter = parsers.into_iter();
+    let mut parser = iter.next().unwrap().boxed();
+    for next in iter {
+        parser = parser.or(next).boxed();
+    }
+    parser
+}
+
+#[tokio::main]
+async fn main() {
+    let text = include_str!("./grammar.sl");
+    let parsed = parse_sygus(text).unwrap();
+    println!("parsed: {:#?}", parsed);
+    let synth_fun = parsed
+        .into_iter()
+        .find_map(|command| {
+            if let Command::SynthFun(fun) = command {
+                Some(fun)
+            } else {
+                None
+            }
+        })
+        .unwrap();
+
+    let parser = synth_fun.parser();
+
+    tracing_subscriber::fmt().init();
+
+    let sampler = GenerationParameters::new();
+
+    let mut llm = LlamaModel::from_builder(
+        Llama::builder().with_source(LlamaSource::qwen_2_5_0_5b_instruct()),
+        ModelLoadingProgress::multi_bar_loading_indicator(),
+    )
+    .await
+    .unwrap();
+
+    tokio::task::spawn_blocking(move || {
+           let mut trie = EvaluationTrie::new();
+             let mut last_entropy = 0.0;
+             let task = r#"Write code for the maximum length of the two strings "hello" and "world". Functions available are: (max2 int int), and (str.len string)."#;
+           for generation in 0.. {
+                let mut session = llm.new_session();
+                let prompt = format!("<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.<|im_end|>\n<|im_start|>user\n{task}<|im_end|>\n<|im_start|>assistant\nHere is the code:\n");
+
+                let output = llm.generate_structured_with_trie(
+                    &mut session,
+                    &prompt,
+                    sampler.clone(),
+                    parser.clone(),
+                    |token| {
+                        print!("{}", token);
+                        std::io::stdout().flush().unwrap();
+                        Ok(())
+                    },
+                    &mut trie,
+                ).unwrap();
+                println!("\n\n");
+
+                println!("generation {generation}:\n{output:?}");
+                let shannon_entropy = trie.shannon_entropy();
+                let entropy_diff = last_entropy - shannon_entropy;
+                println!("entropy diff: {entropy_diff}");
+                if entropy_diff.abs() < 0.00001 {
+                    println!("looks like entropy is converging, stopping generation");
+                    break;
+                }
+                println!("shannon entropy: {shannon_entropy}");
+                last_entropy = shannon_entropy;
+           }
+       }).await.unwrap();
 }
