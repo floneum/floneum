85c027658e595d977313765641a6572164a16d1f
Evan Almloff <evanalmloff@gmail.com>
2025-04-30 21:01:09 -0500

    more candle benchmarks

--- fusor-ml/core/benches/elementwise.rs
+++ fusor-ml/core/benches/elementwise.rs
@@ -2,6 +2,7 @@
 use std::sync::Arc;
 use std::time::Duration;
 
+use candle_core::backend::BackendDevice;
 use criterion::{BatchSize, black_box};
 use fusor_core::{Device, Tensor};
 use futures::executor::block_on;
@@ -61,6 +62,56 @@ fn bench_add_const(c: &mut Criterion) {
                 },
             );
         }
+    }
+
+    {
+        let candle_device = candle_core::Device::Cpu;
+        bench_candle_with_device(candle_device, "add-const-candle-cpu", c);
+    }
+
+    #[cfg(target_os = "macos")]
+    {
+        let candle_device = candle_core::Device::Metal(candle_core::MetalDevice::new(0).unwrap());
+        bench_candle_with_device(candle_device, "add-const-candle-metal", c);
+    }
+}
+
+fn bench_candle_with_device(candle_device: candle_core::Device, name: &str, c: &mut Criterion) {
+    let mut group = c.benchmark_group(name);
+    let group = group.sample_size(20);
+    for size in SIZES {
+        let candle_device = candle_device.clone();
+        group.bench_with_input(BenchmarkId::new(name, size), &size, move |b, &s| {
+            b.to_async(FuturesExecutor).iter_batched(
+                {
+                    let candle_device = candle_device.clone();
+                    let random_data: Vec<Vec<f32>> = (0..size)
+                        .map(|_| (0..size).map(|_| 1.).collect::<Vec<f32>>())
+                        .collect();
+                    move || {
+                        candle_core::Tensor::from_iter(
+                            random_data.iter().flat_map(|x| x.iter().copied()),
+                            &candle_device,
+                        )
+                        .unwrap()
+                        .reshape(&[size, size])
+                        .unwrap()
+                    }
+                },
+                {
+                    let candle_device = candle_device.clone();
+                    move |tensor| {
+                        let candle_device = candle_device.clone();
+                        async move {
+                            let output = (tensor + 1.).unwrap();
+                            candle_device.synchronize().unwrap();
+                            output
+                        }
+                    }
+                },
+                BatchSize::LargeInput,
+            );
+        });
     }
 }
 
--- fusor-ml/core/benches/reduce.rs
+++ fusor-ml/core/benches/reduce.rs
@@ -2,6 +2,7 @@
 use std::sync::Arc;
 use std::time::Duration;
 
+use candle_core::backend::BackendDevice;
 use criterion::BatchSize;
 use fusor_core::Sum;
 use fusor_core::{Device, Tensor};
@@ -59,6 +60,64 @@ fn bench_sum_reduce(c: &mut Criterion) {
                 },
             );
         }
+    }
+    
+    {
+        let candle_device = candle_core::Device::Cpu;
+        bench_candle_with_device(
+            candle_device,
+            "sum-candle-cpu",
+            c,
+        );
+    }
+
+    #[cfg(target_os = "macos")]
+    {
+        let candle_device = candle_core::Device::Metal(candle_core::MetalDevice::new(0).unwrap());
+        bench_candle_with_device(candle_device, "sum-candle-metal", c);
+    }
+}
+
+fn bench_candle_with_device(candle_device: candle_core::Device, name: &str, c: &mut Criterion) {
+    let mut group = c.benchmark_group(name);
+    let group = group.sample_size(20);
+    for size in SIZES {
+        let candle_device = candle_device.clone();
+        group.bench_with_input(
+            BenchmarkId::new(name, size),
+            &size,
+            move |b, &s| {
+                b.to_async(FuturesExecutor).iter_batched(
+                    {
+                        let candle_device = candle_device.clone();
+                        let random_data: Vec<Vec<f32>> = (0..size)
+                            .map(|_| (0..size).map(|_| 1.).collect::<Vec<f32>>())
+                            .collect();
+                        move || {
+                            candle_core::Tensor::from_iter(
+                                random_data.iter().flat_map(|x| x.iter().copied()),
+                                &candle_device,
+                            )
+                            .unwrap()
+                            .reshape(&[size, size])
+                            .unwrap()
+                        }
+                    },
+                    {
+                        let candle_device = candle_device.clone();
+                        move |tensor| {
+                            let candle_device = candle_device.clone();
+                            async move {
+                                let output = tensor.sum(0).unwrap();
+                                candle_device.synchronize().unwrap();
+                                output
+                            }
+                        }
+                    },
+                    BatchSize::LargeInput,
+                );
+            },
+        );
     }
 }
 
