60c9e6acce0651255de242bcb16d71589b7b0780
Daniel Frederico Lins Leite <xunilrj@hotmail.com>
2025-06-28 10:54:33 -0300

    convert vocab to AHashMap (#389)

--- models/kalosm-llama/src/gguf_tokenizer.rs
+++ models/kalosm-llama/src/gguf_tokenizer.rs
@@ -155,6 +155,7 @@ impl GGUFPreTokenizerConfig {
             })
             .collect();
         let bos_token = vocab[bos];
+        let vocab = ahash::AHashMap::from_iter(vocab.into_iter());
         let bpe_tokenizer = BpeBuilder::new()
             .vocab_and_merges(vocab, merges)
             .ignore_merges(self.ignore_merges)
