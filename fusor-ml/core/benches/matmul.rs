d53a1f0bf3afd0b53f584e76581814bcb518c729
Evan Almloff <evanalmloff@gmail.com>
2025-11-02 13:49:19 -0600

    more matmul bench shapes

--- fusor-ml/core/benches/matmul.rs
+++ fusor-ml/core/benches/matmul.rs
@@ -12,15 +12,19 @@ use criterion::{criterion_group, criterion_main};
 
 use criterion::async_executor::FuturesExecutor;
 
-const SIZES: [[usize; 2]; 8] = [
-    [128, 128],
-    [128, 1],
-    [1024, 1024],
-    [1024, 1],
-    [2048, 2048],
-    [2048, 1],
-    [4096, 4096],
-    [4096, 1],
+const SIZES: [[usize; 3]; 12] = [
+    [1, 1500, 64],
+    [64, 1500, 1],
+    [1, 2048, 64],
+    [64, 2048, 1],
+    [128, 128, 128],
+    [1, 128, 1],
+    [1024, 1024, 1024],
+    [1, 1024, 1],
+    [2048, 2048, 2048],
+    [1, 2048, 1],
+    [4096, 4096, 4096],
+    [1, 4096, 1],
 ];
 
 fn matmul(c: &mut Criterion) {
@@ -29,20 +33,22 @@ fn matmul(c: &mut Criterion) {
 
         let device = block_on(Device::new()).unwrap();
 
-        for [m, n] in SIZES {
+        for [m, k, n] in SIZES {
             let device = device.clone();
             group.bench_with_input(
-                BenchmarkId::new("matmul-wgpu", format!("{}x{}", m, n)),
-                &(m, n),
-                move |b, &(m, n)| {
+                BenchmarkId::new("matmul-wgpu", format!("{m}x{k} by {k}x{n}")),
+                &(m, k, n),
+                move |b, &(m, k, n)| {
                     let device = device.clone();
                     b.to_async(FuturesExecutor).iter_custom(async |iters| {
                         let mut sum = Duration::ZERO;
                         while sum.is_zero() {
                             for _ in 0..iters {
-                                let tensor = Tensor::new(&device, &vec![vec![1.; n]; m]);
-                                _ = tensor.as_slice().await.unwrap();
-                                let new = tensor.mat_mul(&tensor.t());
+                                let tensor1 = Tensor::new(&device, &vec![vec![1.; k]; m]);
+                                let tensor2 = Tensor::new(&device, &vec![vec![1.; n]; k]);
+                                _ = tensor1.as_slice().await.unwrap();
+                                _ = tensor2.as_slice().await.unwrap();
+                                let new = tensor1.mat_mul(&tensor2);
                                 let start = std::time::Instant::now();
                                 new.materialize().await;
                                 sum += start.elapsed();
@@ -55,26 +61,10 @@ fn matmul(c: &mut Criterion) {
         }
     }
 
+    #[cfg(target_os = "macos")]
     {
-        let mut group = c.benchmark_group("matmul-ndarray");
-        let group = group.sample_size(20);
-
-        for [m, n] in SIZES {
-            group.bench_with_input(
-                BenchmarkId::new("matmul-ndarray", format!("{}x{}", m, n)),
-                &(m, n),
-                move |b, &(m, n)| {
-                    b.to_async(FuturesExecutor).iter_batched(
-                        || {
-                            let matrix = ndarray::Array2::<f32>::ones((m, n));
-                            (matrix.clone(), matrix.clone())
-                        },
-                        |(tensor_a, tensor_b)| async move { tensor_a.dot(&tensor_b.t()) },
-                        BatchSize::LargeInput,
-                    );
-                },
-            );
-        }
+        let candle_device = candle_core::Device::Metal(candle_core::MetalDevice::new(0).unwrap());
+        bench_candle_with_device(candle_device, "matmul-candle-metal", c);
     }
 
     {
@@ -82,44 +72,73 @@ fn matmul(c: &mut Criterion) {
         bench_candle_with_device(candle_device, "matmul-candle-cpu", c);
     }
 
-    #[cfg(target_os = "macos")]
     {
-        let candle_device = candle_core::Device::Metal(candle_core::MetalDevice::new(0).unwrap());
-        bench_candle_with_device(candle_device, "matmul-candle-metal", c);
+        let mut group = c.benchmark_group("matmul-ndarray");
+        let group = group.sample_size(20);
+
+        for [m, k, n] in SIZES {
+            group.bench_with_input(
+                BenchmarkId::new("matmul-ndarray", format!("{m}x{k} by {k}x{n}")),
+                &(m, k, n),
+                move |b, &(m, k, n)| {
+                    b.to_async(FuturesExecutor).iter_batched(
+                        || {
+                            let matrix1 = ndarray::Array2::<f32>::ones((m, k));
+                            let matrix2 = ndarray::Array2::<f32>::ones((k, n));
+                            (matrix1, matrix2)
+                        },
+                        |(tensor_a, tensor_b)| async move { tensor_a.dot(&tensor_b) },
+                        BatchSize::LargeInput,
+                    );
+                },
+            );
+        }
     }
 }
 
 fn bench_candle_with_device(candle_device: candle_core::Device, name: &str, c: &mut Criterion) {
     let mut group = c.benchmark_group(name);
     let group = group.sample_size(20);
-    for [m, n] in SIZES {
+    for [m, k, n] in SIZES {
         let candle_device = candle_device.clone();
         group.bench_with_input(
-            BenchmarkId::new(name, format!("{}x{}", m, n)),
-            &(m, n),
-            move |b, &(m, n)| {
+            BenchmarkId::new(name, format!("{m}x{k} by {k}x{n}")),
+            &(m, k, n),
+            move |b, &(m, k, n)| {
                 b.to_async(FuturesExecutor).iter_batched(
                     {
                         let candle_device = candle_device.clone();
-                        let random_data: Vec<Vec<f32>> = (0..m)
+                        let random_data_1: Vec<Vec<f32>> = (0..m)
+                            .map(|_| (0..k).map(|_| 1.).collect::<Vec<f32>>())
+                            .collect();
+                        let random_data_2: Vec<Vec<f32>> = (0..k)
                             .map(|_| (0..n).map(|_| 1.).collect::<Vec<f32>>())
                             .collect();
                         move || {
-                            candle_core::Tensor::from_iter(
-                                random_data.iter().flat_map(|x| x.iter().copied()),
-                                &candle_device,
+                            (
+                                candle_core::Tensor::from_iter(
+                                    random_data_1.iter().flat_map(|x| x.iter().copied()),
+                                    &candle_device,
+                                )
+                                .unwrap()
+                                .reshape(&[m, k])
+                                .unwrap(),
+                                candle_core::Tensor::from_iter(
+                                    random_data_2.iter().flat_map(|x| x.iter().copied()),
+                                    &candle_device,
+                                )
+                                .unwrap()
+                                .reshape(&[k, n])
+                                .unwrap(),
                             )
-                            .unwrap()
-                            .reshape(&[m, n])
-                            .unwrap()
                         }
                     },
                     {
                         let candle_device = candle_device.clone();
-                        move |tensor| {
+                        move |(tensor1, tensor2)| {
                             let candle_device = candle_device.clone();
                             async move {
-                                let output = tensor.matmul(&tensor.t().unwrap()).unwrap();
+                                let output = tensor1.matmul(&tensor2).unwrap();
                                 candle_device.synchronize().unwrap();
                                 output
                             }
