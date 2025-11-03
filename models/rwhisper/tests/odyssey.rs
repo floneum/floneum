6f8e2fe3fa035cc6609055e7357fd0e91fef78c3
Evan Almloff <evanalmloff@gmail.com>
2025-11-02 21:27:42 -0600

    fix cargo check

--- models/rwhisper/tests/odyssey.rs
+++ models/rwhisper/tests/odyssey.rs
@@ -13,7 +13,7 @@ async fn transcribe_the_odyssey() -> Result<(), anyhow::Error> {
 
     // Create a new small whisper model
     let model = WhisperBuilder::default()
-        .with_source(WhisperSource::QuantizedTinyEn)
+        .with_source(WhisperSource::tiny_en())
         .build()
         .await?;
 
