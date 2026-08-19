9f52185c23ba716ff0b245c992e0f95b3dbfa78f
D4RKAR <D4RKAR117@outlook.es>
2026-08-18 22:56:18 -0500

    Revert GGUF tokenizer backport mismatch

--- models/kalosm-llama/src/gguf_tokenizer.rs
+++ models/kalosm-llama/src/gguf_tokenizer.rs
@@ -155,7 +155,6 @@ impl GGUFPreTokenizerConfig {
             })
             .collect();
         let bos_token = vocab[bos];
-        let vocab = ahash::AHashMap::from_iter(vocab.into_iter());
         let bpe_tokenizer = BpeBuilder::new()
             .vocab_and_merges(vocab, merges)
             .ignore_merges(self.ignore_merges)
